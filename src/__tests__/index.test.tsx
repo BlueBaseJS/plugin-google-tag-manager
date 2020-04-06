7811ef696be5be9840f31d32c3af6501188e8eb2
Hashir Ch <info@iammhc.com>
2020-04-06 11:30:46 +0500

    fix(gtm): initialize and dataLayer add separately

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -3,6 +3,7 @@ import Plugin from '../index';
 import TagManager from 'react-gtm-module';
 jest.mock('react-gtm-module', () => ({
 	initialize: jest.fn(),
+	dataLayer: jest.fn(),
 }));
 test('Plugin should be correctly registered', async () => {
 	const BB = new BlueBase();
@@ -10,11 +11,38 @@ test('Plugin should be correctly registered', async () => {
 	expect(BB.Plugins.has('plugin-google-tag-manager')).toBeTruthy();
 });
 
-test('Google Tag Manager Fillter Run', async () => {
+test('Google Tag Manager Fillter Run but not Initialize Called on Native', async () => {
 	const BB = new BlueBase();
 	await BB.boot({
 		plugins: [Plugin],
 	});
 	await BB.Filters.run('bluebase.analytics.track', { name: 'albumVisit' });
+	expect(TagManager.initialize).toBeCalledTimes(0);
+});
+
+test('Google Tag Manager Fillter Run and Initialize Called on web', async () => {
+	jest.mock('Platform', () => {
+		const Platform = require.requireActual('Platform');
+		Platform.OS = 'web';
+		return Platform;
+	});
+	const BB = new BlueBase();
+	await BB.boot({
+		plugins: [Plugin],
+	});
+	await BB.Filters.run('bluebase.analytics.track', null);
 	expect(TagManager.initialize).toBeCalled();
+});
+test('Google Tag Manager Fillter Run and datalayer Called on web', async () => {
+	jest.mock('Platform', () => {
+		const Platform = require.requireActual('Platform');
+		Platform.OS = 'web';
+		return Platform;
+	});
+	const BB = new BlueBase();
+	await BB.boot({
+		plugins: [Plugin],
+	});
+	await BB.Filters.run('bluebase.analytics.track', { name: 'albumVisit' });
+	expect(TagManager.dataLayer).toBeCalled();
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { Platform } from 'react-native';
 import TagManager from 'react-gtm-module';
 import { createPlugin } from '@bluebase/core';
 import get from 'lodash.get';
@@ -5,15 +6,22 @@ export default createPlugin({
 	categories: ['analytics'],
 	description: 'This plugin allows allows you manage and deploy marketing tags',
 	filters: {
-		'bluebase.analytics.track': (data, _ctx, BB) => {
-			const tagManagerArgs = {
-				...data,
-				auth: get(BB.Configs.get('plugin.gtm.auth'), 'value'),
-				gtmId: get(BB.Configs.get('plugin.gtm.id'), 'value'),
-				preview: get(BB.Configs.get('plugin.gtm.preview'), 'value'),
-			};
-
-			TagManager.initialize(tagManagerArgs);
+		'bluebase.analytics.track': (dataLayer, _ctx, BB) => {
+			if (Platform.OS === 'web') {
+				if (dataLayer) {
+					const tagManagerArgs = {
+						dataLayer,
+					};
+					TagManager.dataLayer(tagManagerArgs);
+				} else {
+					const tagManagerArgs = {
+						auth: get(BB.Configs.get('plugin.gtm.auth'), 'value'),
+						gtmId: get(BB.Configs.get('plugin.gtm.id'), 'value'),
+						preview: get(BB.Configs.get('plugin.gtm.preview'), 'value'),
+					};
+					TagManager.initialize(tagManagerArgs);
+				}
+			}
 		},
 	},
 	key: 'plugin-google-tag-manager',
